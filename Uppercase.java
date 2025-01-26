3d15c4b86549bd9225c8ef780d4f4c8c0e832dec
Shaiksaniya06 <sksaniya2006@gmail.com>
2025-01-26 13:11:01 +0530

    Add files via upload

---
+++ Uppercase.java
@@ -0,0 +1,13 @@
+public class Uppercase {
+    public static void main(String[] args) {
+        String str ="saniya";
+        System.out.println(str.toUpperCase());
+    }
+}
+
+    
+        
+    
+
+    
+
